c8d6c293ff347595248f98dfcabb04bf983457cc
Himanshu Chandekar <124911396+llhimanshull@users.noreply.github.com>
2026-01-21 02:33:09 +0530

    initial commit

---
+++ src/main/java/com/carrexa/carrexa/CarrexaApplication.java
@@ -0,0 +1,13 @@
+package com.carrexa.carrexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrexaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carrexa/carrexa/CarrexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrexa.carrexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
